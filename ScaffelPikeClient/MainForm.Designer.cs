2f137813f31ee39c11fbf3cb38c44692ba37dcaa
Joe Osborne <joe.osborne1337@gmail.com>
2022-01-25 22:13:31 +0000

    Load data using combo boxes. Todo load data

--- ScaffelPikeClient/MainForm.Designer.cs
+++ ScaffelPikeClient/MainForm.Designer.cs
@@ -28,22 +28,72 @@
     /// </summary>
     private void InitializeComponent()
     {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.comboBoxDatabase = new System.Windows.Forms.ComboBox();
+            this.comboBoxDataset = new System.Windows.Forms.ComboBox();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
             this.SuspendLayout();
+            // 
+            // comboBoxDatabase
+            // 
+            this.comboBoxDatabase.FormattingEnabled = true;
+            this.comboBoxDatabase.Location = new System.Drawing.Point(28, 57);
+            this.comboBoxDatabase.Name = "comboBoxDatabase";
+            this.comboBoxDatabase.Size = new System.Drawing.Size(247, 21);
+            this.comboBoxDatabase.TabIndex = 0;
+            this.comboBoxDatabase.SelectedValueChanged += new System.EventHandler(this.comboBoxDatabase_SelectedValueChanged);
+            // 
+            // comboBoxDataset
+            // 
+            this.comboBoxDataset.FormattingEnabled = true;
+            this.comboBoxDataset.Location = new System.Drawing.Point(281, 57);
+            this.comboBoxDataset.Name = "comboBoxDataset";
+            this.comboBoxDataset.Size = new System.Drawing.Size(236, 21);
+            this.comboBoxDataset.TabIndex = 1;
+            this.comboBoxDataset.SelectedValueChanged += new System.EventHandler(this.comboBoxDataset_SelectedValueChanged);
+            // 
+            // chart1
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(28, 116);
+            this.chart1.Name = "chart1";
+            series1.ChartArea = "ChartArea1";
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            this.chart1.Series.Add(series1);
+            this.chart1.Size = new System.Drawing.Size(489, 300);
+            this.chart1.TabIndex = 2;
+            this.chart1.Text = "chart1";
+            this.chart1.Click += new System.EventHandler(this.chart1_Click);
             // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(549, 450);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.comboBoxDataset);
+            this.Controls.Add(this.comboBoxDatabase);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "MainForm";
-            this.Text = "MainForm";
+            this.Text = "Timeseries Charts";
             this.Load += new System.EventHandler(this.MainForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
             this.ResumeLayout(false);
 
     }
 
     #endregion
+
+    private System.Windows.Forms.ComboBox comboBoxDatabase;
+    private System.Windows.Forms.ComboBox comboBoxDataset;
+    private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
   }
 }
--- ScaffelPikeClient/MainForm.cs
+++ ScaffelPikeClient/MainForm.cs
@@ -1,11 +1,19 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Windows.Forms;
+using ScaffelPikeContracts;
 
 namespace ScaffelPikeClient
 {
   public partial class MainForm : Form
   {
+    private List<DatabaseResponse> Databases { get; set; }
+    private DatabaseResponse DatabaseSelected { get; set; }
+    private List<DatasetResponse> Datasets{ get; set; }
+    private DatasetResponse DatasetSelected { get; set; }
+    private MyTimeseriesDataResponse TimeseriesData { get; set; }
+
     public MainForm()
     {
       InitializeComponent();
@@ -13,11 +21,53 @@ namespace ScaffelPikeClient
 
     private async void MainForm_Load(object sender, EventArgs e)
     {
-      var databases = await ClientRefs.ScaffelPikeChannel.GetQuandlDbs();
-      var dbToUse = databases.First(db => db.Code.Contains("BITFINEX"));
-      var datasets = await ClientRefs.ScaffelPikeChannel.GetQuandlDataSets(dbToUse.Code);
-      var dsToUse = datasets.First(ds => ds.Code.Contains("BTC") && ds.Code.Contains("ETH"));
-      var timeseries = await ClientRefs.ScaffelPikeChannel.GetQuandlTimeseries(dbToUse.Code, dsToUse.Code);  // ~0.5mb 
+      Databases = await ClientRefs.ScaffelPikeChannel.GetQuandlDbs();
+      //var dbToUse = databases.First(db => db.Code.Contains("BITFINEX"));
+      //var datasets = await ClientRefs.ScaffelPikeChannel.GetQuandlDataSets(dbToUse.Code);
+      //var dsToUse = datasets.First(ds => ds.Code.Contains("BTC") && ds.Code.Contains("ETH"));
+      //var timeseries = await ClientRefs.ScaffelPikeChannel.GetQuandlTimeseries(dbToUse.Code, dsToUse.Code);  // ~0.5mb 
+
+      comboBoxDatabase.Items.AddRange(Databases.Select(db => db.Code).ToArray());
+    }
+
+    private void chart1_Click(object sender, EventArgs e)
+    {
+
+    }
+
+    private async void comboBoxDatabase_SelectedValueChanged(object sender, EventArgs e)
+    {
+      comboBoxDataset.Items.Clear();
+      comboBoxDataset.ResetText();
+
+      var dbCodeToUse = (string)comboBoxDatabase.SelectedItem;
+      DatabaseSelected = Databases.FirstOrDefault(db => db.Code == dbCodeToUse);
+
+      try
+      {
+        Datasets = await ClientRefs.ScaffelPikeChannel.GetQuandlDataSets(dbCodeToUse);
+        comboBoxDataset.Items.AddRange(Datasets.Select(ds => ds.Code).ToArray());
+      }
+      catch(Exception ex)
+      {
+        ClientRefs.Log.Error("comboBoxDatabase_SelectedValueChanged", ex);
+      }
+    }
+
+    private async void comboBoxDataset_SelectedValueChanged(object sender, EventArgs e)
+    {
+      var dbCodeToUse = (string)comboBoxDatabase.SelectedItem;
+      var dsCodeToUse = (string)comboBoxDataset.SelectedItem;
+      DatasetSelected = Datasets.FirstOrDefault(ds => ds.Code == dsCodeToUse);
+
+      try
+      {
+        TimeseriesData = await ClientRefs.ScaffelPikeChannel.GetQuandlTimeseries(dbCodeToUse, dsCodeToUse);
+      }
+      catch (Exception ex)
+      {
+        ClientRefs.Log.Error("comboBoxDataset_SelectedValueChanged", ex);
+      }
     }
   }
 }
