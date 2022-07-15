b844ce5135fee6cfabd879da9af1594089514f81
Joe Osborne <joe.osborne1337@gmail.com>
2022-07-15 08:15:34 +0100

    Tweak Test to account for explicit down factor

--- ScaffelPikeTests/Derivatives/Factory/BinaryTreeFactoryTests.cs
+++ ScaffelPikeTests/Derivatives/Factory/BinaryTreeFactoryTests.cs
@@ -14,16 +14,21 @@ namespace ScaffelPikeTests.Derivatives.Factory
     [InlineData(4, 3, 2)]
     [InlineData(4, 10, 2)]
     [InlineData(4, 2, 1.1)]
-    public void GeneratePathCombinationsForTime(int So, int N, double u)
+    [InlineData(4, 3, 2, 0.33)]
+    [InlineData(4, 10, 2, 0.8)]
+    [InlineData(4, 2, 1.1 , 0.1)]
+
+    public void GeneratePathCombinationsForTime(int So, int N, double u, double d = -1)
     {
       //arrange 
 
       //act 
-      var tree = BinaryTreeFactory.CreateSecurityTree(So, N, u);
+      var tree = d == -1 ? BinaryTreeFactory.CreateSecurityTree(So, N, u) : BinaryTreeFactory.CreateSecurityTree(So, N, u, d);
       //assert
+      var exMin = d == -1 ? So * Math.Pow(u, -N) : So * Math.Pow(d, N);
       Assert.Equal(tree.Count, Enumerable.Range(0, N + 1).Sum(i => Combinations.summedNCR(i)));
       Assert.Equal(So * Math.Pow(u, N), tree.MaxInTree().Select(n => n.Data).Max());
-      Assert.Equal(So * Math.Pow(u, -N), tree.MinInTree().Select(n => n.Data).Min());
+      Assert.Equal(exMin, tree.MinInTree().Select(n => n.Data).Min());
     }
   }
 }
