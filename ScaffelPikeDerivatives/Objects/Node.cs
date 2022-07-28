482b5cb0e1229ca3d32c46778812cafa2f73d2c2
Joe Osborne <joe.osborne1337@gmail.com>
2022-07-28 22:24:09 +0100

    Revert "Solution and projectName changes"
    This reverts commit 77166eb1017d3467015052a9b09f36ab1867a8d7.

--- ScaffelPikeDerivatives/Objects/Node.cs
+++ ScaffelPikeDerivatives/Objects/Node.cs
@@ -5,10 +5,10 @@ using System.Linq;
 using System.Runtime.Serialization;
 using System.Text;
 using System.Threading.Tasks;
-using ScafellPikeDerivatives.Objects.Interfaces;
-using ScafellPikeDerivatives.Objects.Iterators;
+using ScaffelPikeDerivatives.Objects.Interfaces;
+using ScaffelPikeDerivatives.Objects.Iterators;
 
-namespace ScafellPikeDerivatives.Objects
+namespace ScaffelPikeDerivatives.Objects
 {
   [DataContract]
   public class Node<T> : INode<T> where T : IEquatable<T>
